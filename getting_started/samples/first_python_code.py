5187d7ece4a5a6c2530957aafe660ed80b17c3bb
Karen Cano <karenirene.cano1203@gmail.com>
2018-09-08 13:58:07 +0800

    Assign my name on the your_name variable

--- getting_started/samples/first_python_code.py
+++ getting_started/samples/first_python_code.py
@@ -5,7 +5,7 @@
 organization = 'WomenWhoCode!'
 
 # You can print it right away!
-print('Hello WomanWhoCode!This is Karen\'s first python code :D')
+print('Hello WomanWhoCode!')
 
 # Example of string concatenation
 print('Hello ' + organization)
@@ -15,7 +15,7 @@ print('Hello {}'.format(organization))
 # print(f'Hello {organization}')  # Can use this format with Python3.6
 
 # First, replace the following text with your name
-your_name = '<place your name here>'
+your_name = 'Karen Cano'
 
 # Then, uncomment the next line by removing the "#" sign
-#print('{} loves Python!'.format(your_name))
+print('{} loves Python!'.format(your_name))
